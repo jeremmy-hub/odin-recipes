e5d17a7c8845d39cfe15848664241a7112a18407
jey_kali <jeremymwas4@gmail.com>
2023-08-06 05:23:05 +0300

    modified the UI of the game

--- projects/rock_scissors_game.js
+++ projects/rock_scissors_game.js
@@ -2,60 +2,101 @@
 // possible moves Rock paper scissors
 // possible outcomes win lose draw
 const GAME_CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
+const computerButtons = document.querySelectorAll('.computers');
+const playerButtons = document.querySelectorAll('.player1');
+const gameStartBtn = document.querySelector('.start_game');
+const INSTRUCTIONS_PANEL = document.querySelector('.instructions_container');
+const FINAL_RESULTS_PANEL = document.querySelector('.textBox')
+const scores_history = document.querySelector('.individual_game_container');
 
+async function getGameChoices() {
+  return new Promise((resolve, reject) => {
+    function choiceClickHandler(event) {
+      INSTRUCTIONS_PANEL.textContent = `You selected ${event.target.textContent.toLowerCase()}`;
+      
+      for (let btn of playerButtons) {btn.removeEventListener('click', choiceClickHandler);}
+      
+      setTimeout(() => {resolve(event.target.textContent.toLowerCase());},1000);}
 
-function getPlayerChoice(){
-    let player_choice = prompt("Please choose between Rock, scissors, or Paper: ");
-    return player_choice.toLowerCase();
-}
+    for (let btn of playerButtons) {
+      btn.addEventListener('click', choiceClickHandler);
+    }
+  });}
 
-function getCompChoice(){
+async function getComputerChoices(){
+    let choice = await getCompChoice();
+    return new Promise((resolve, reject)=>{
+             setTimeout(()=>{resolve(choice);},1000)});}
+
+async function getCompChoice(){
     let choice_index = Math.floor((Math.random() * 3) + 0 );
-    return GAME_CHOICES[choice_index].toLowerCase();
+    let choice = GAME_CHOICES[choice_index].toLowerCase();
+    
+    INSTRUCTIONS_PANEL.textContent = `The computer selected ${choice}`;
+    return choice;
 }
 
-function EvaluateChoices(person_score,computers_score){
-    let persons_choice = '', computers_choice = '';
-    try{
-        persons_choice = getPlayerChoice();
-        computers_choice = getCompChoice();
-    
-        if(persons_choice === 'rock' && computers_choice === 'scissors'){
-            person_score ++;
-        }
-        else if(persons_choice === 'scissors' && computers_choice === 'rock')
-        {computers_score ++;}
 
-        else if(persons_choice === 'paper' && computers_choice === 'rock')
-        {person_score ++;}
+function EvaluateChoices(person_score,computers_score, computers_choice, persons_choice){
+    let explanation = '';
+    if(persons_choice === 'rock' && computers_choice === 'scissors'){
+        explanation = 'its a win';
+        person_score ++;
+    }
+    else if(persons_choice === 'scissors' && computers_choice === 'rock'){
+        explanation = 'its a lose';
+        computers_score ++;
+    }
 
-        else if(persons_choice === 'rock' && computers_choice === 'paper')
-        {computers_score ++;}
+    else if(persons_choice === 'paper' && computers_choice === 'rock'){
+        explanation = 'its a win';
+        person_score ++;}
 
-        else if(persons_choice ==='scissors' && computers_choice === 'paper'){person_score ++;}
+    else if(persons_choice === 'rock' && computers_choice === 'paper'){
+        explanation = 'its a lose';
+        computers_score ++;
+    }
 
-        else if(persons_choice === 'paper' && computers_choice === 'scissors'){ computers_score ++;}
+    else if(persons_choice ==='scissors' && computers_choice === 'paper'){
+        explanation = 'its a win';
+        person_score++;
+    }
+
+    else if(persons_choice === 'paper' && computers_choice === 'scissors'){
+        explanation = 'its a lose';
+        computers_score ++;}
         
-        else{
-            computers_score ++;
-            person_score ++;
-        }
-
+    else{
+        explanation = 'its a draw'
+        computers_score ++;
+        person_score ++;
     }
-    catch (e) {
-        console.log(e.message)// TODO: handle exception
-    }
+    scores_history.innerHTML += ` &gt; current scores ${person_score} : ${computers_score} -- ${explanation}<br/>`
     return {person_score, computers_score}
 }
 
-function HandleGameTrials(){
-    let trials = 0, person_score = 0, computers_score = 0;
+async function HandleGameTrials(){
+    let trials = 0;
+    let person_score = 0;
+    let computers_score = 0;
+
     while(trials < 5){
-        const updatedScores = EvaluateChoices(person_score, computers_score);
+        let persons_choice = '';
+        let computers_choice = '';
+        try{
+            persons_choice = await getGameChoices();
+            computers_choice = await getComputerChoices();
+        }
+        catch (error){
+            console.log(error)
+        }
+        
+        const updatedScores = EvaluateChoices(person_score, computers_score, computers_choice, persons_choice);
         person_score = updatedScores.person_score;
         computers_score = updatedScores.computers_score;
         trials ++
     }
+    INSTRUCTIONS_PANEL.textContent = '';
     return [person_score, computers_score];
 }
 
@@ -74,10 +115,24 @@ function determineWinner(person_score, computers_score) {
     return results;
 }
 
-function PlayGame(){
-    let scores = HandleGameTrials();
+function forceHover(element){
+    element.classList.add('hover-effect')
+}
+function removeHover(element) {
+  element.classList.remove('hover-effect');
+}
+
+async function PlayGame(){
+    let scores = await HandleGameTrials();
     let game_scores = determineWinner(...scores);
-    alert(game_scores);
+    FINAL_RESULTS_PANEL.textContent =  game_scores;
+    setTimeout(()=>{
+        window.location.reload();
+    },10000);
     return;
 }
 
+gameStartBtn.addEventListener('click',()=>{
+    FINAL_RESULTS_PANEL.textContent = '';
+    PlayGame();
+})
